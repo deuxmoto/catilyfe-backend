4bcddd91975500d2a591037e592c41ec2354f857
Peter Sulucz <peter.sulucz@gmail.com>
2018-01-15 00:46:32 -0500

    Forgot tests

---
+++ Unit/CoreTests/ImportantTest.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace CoreTests
+{
+    public class ImportantTest
+    {
+        /// <summary>
+        /// Please work.
+        /// </summary>
+        [Fact]
+        public void InitialTest()
+        {
+            Assert.True(true);
+        }
+    }
+}

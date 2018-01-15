264526ca75fe0efa5e0623d42425c116cfb7dffb
Peter Sulucz <peter.sulucz@gmail.com>
2018-01-15 11:42:31 -0800

    Changing build drastically. Switching UT project type to MStest

---
+++ Unit/CatiLyfe.Common.MSTest/PasswordGeneratorTests.cs
@@ -0,0 +1,26 @@
+﻿namespace CatiLyfe.Common.MSTest
+{
+    using CatiLyfe.Common.Security;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+
+    /// <summary>
+    /// Tests for the password generator.
+    /// </summary>
+    [TestClass]
+    public class PasswordGeneratorTests
+    {
+        /// <summary>
+        /// Test to see if the random bytes stuff works.
+        /// </summary>
+        [TestMethod]
+        public void TestGetRandomBytes()
+        {
+            var generator = new PasswordGenerator("asdf");
+            var bytes = generator.GenerateTokenBytes(100);
+
+            Assert.AreEqual(100, bytes.Length);
+            Assert.IsTrue(bytes.Any(b => b != 0));
+        }
+    }
+}

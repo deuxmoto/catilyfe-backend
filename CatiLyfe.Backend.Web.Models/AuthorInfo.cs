a6403095734c40dce4abfc8fa7c33230dcf8d488
petersulucz <peter.sulucz@gmail.com>
2018-01-21 18:10:15 -0800

    Added the author stuff yo. (#10)

---
+++ CatiLyfe.Backend.Web.Models/AuthorInfo.cs
@@ -0,0 +1,29 @@
+﻿namespace CatiLyfe.Backend.Web.Models
+{
+    /// <summary>
+    /// The author info.
+    /// </summary>
+    public class AuthorInfo
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthorInfo"/>.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="id"></param>
+        public AuthorInfo(string name, int id)
+        {
+            this.Name = name;
+            this.UserId = id;
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the user id.
+        /// </summary>
+        public int UserId { get; }
+    }
+}

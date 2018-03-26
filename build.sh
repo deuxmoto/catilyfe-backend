b32f472e05c1abc846165fd97edf01676bcff171
petersulucz <peter.sulucz@gmail.com>
2018-03-26 00:13:54 -0400

    DONT CLOSE THIS WHOPPER (#21)
    * test 1
    
    * flipped her
    
    * flipped case
    
    * flipped case
    
    * flipped case
    
    * At this point just trying shit
    
    * At this point just trying shit 2
    
    * At this point just trying shit 3
    
    * At this point just trying shit 4
    
    * At this point just trying shit 5
    
    * At this point just trying shit 6
    
    * At this point just trying shit 7
    
    * At this point just trying shit 8
    
    * Ok now trying to cs i guess
    
    * Ok now trying to cs i guess 2
    
    * Ok now trying to cs i guess 3
    
    * Ok now trying to cs i guess 4
    
    * Ok now trying to cs i guess 5
    
    * publish?
    
    * YEEEEEEEEEEEE
    
    * DEPLOY THIS LIL GUY
    
    * Complicated now

--- build.sh
+++ build.sh
@@ -1,3 +1,44 @@
 #!/bin/bash
 
-dotnet restore && dotnet build
+echo "DOIN SOME RESTORIN"
+dotnet restore
+echo "AIGH ALL RESTORED"
+
+if [ $TRAVIS_PULL_REQUEST == "false" ]
+then
+    echo "AIIIGHHTTT NOW WE CAN DEPLOY YO."
+    echo "BUILDIN THAT RELEASE MODE YO"
+    dotnet build -c Release
+else
+    echo "AIIIGHHTTT WE PULLIN. SO WE AINT DEPLOYIN."
+    echo "WE BUILDIN DEBUG"
+    dotnet build -c Debug
+fi
+echo "AIGHT SHE ALL BUILT UP"
+
+
+echo "RUNNIN SOME TESTIES"
+
+# Runs tests on on test files
+for PROJ in `find -name *MSTest.csproj`;
+    do
+    echo $PROJ
+	dotnet test $PROJ
+    done
+
+echo "TESTIES are all done"
+
+if [ $TRAVIS_PULL_REQUEST == "false" ]
+then
+    cd ./CatiLyfe.Backend.Web.Core
+    dotnet publish -c Release
+    cd ./bin/Release/netcoreapp2.0/publish
+    git init
+    DEPLOY_URL="https://$CL_AZURE_DEPLOYMENT_USER:$CL_AZURE_DEPLOYMENT_USER_PASSWORD@caticake.scm.azurewebsites.net/caticake.git"
+    echo "$DEPLOY_URL"
+    git remote add azure "$DEPLOY_URL"
+    git add .
+    git commit -m "Deployin this lil guy."
+    git push -f azure master
+    echo "ALL DONE NOW"
+fi
